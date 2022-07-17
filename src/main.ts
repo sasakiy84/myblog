35ad5cd434611a877f33fd3b4c5502f4a70077e0
sasakiy84 <sasakiy84@g.ecc.u-tokyo.ac.jp>
2022-07-17 19:09:23 +0900

    add ssg feature

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,73 @@
-import { createApp } from "vue";
 import "./normalize.css";
 import "./util.scss";
 import App from "./App.vue";
+import { ViteSSG } from "vite-ssg";
 
-import { createRouter, createWebHistory } from "vue-router";
 import MarkdownViewVue from "./pages/MarkdownView.vue";
 import ArticleIndex from "./pages/ArticleIndex.vue";
 import Profile from "./pages/Profile.vue";
 import Top from "./pages/Top.vue";
 import Project from "./pages/ProjectIndex.vue";
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: "/articles",
-      component: ArticleIndex,
-    },
-    {
-      path: "/articles/:title",
-      component: MarkdownViewVue,
-    },
-    {
-      path: "/profile",
-      component: Profile,
-    },
-    {
-      path: "/projects",
-      component: Project,
-    },
-    {
-      path: "/",
-      component: Top,
-    },
-  ],
-});
-createApp(App).use(router).mount("#app");
+import { RouteRecordRaw } from "vue-router";
+
+// const jsonData = await readFile(join("public", "contents.json"));
+// const {
+//   articles,
+// }: {
+//   articles: { baseFileName: string; title: string; description: string }[];
+// } = JSON.parse(jsonData.toString());
+const routes: RouteRecordRaw[] = [
+  {
+    name: "articleIndex",
+    path: "/articles",
+    component: ArticleIndex,
+  },
+  {
+    name: "articles",
+    path: "/articles/:title",
+    component: MarkdownViewVue,
+  },
+  {
+    name: "profile",
+    path: "/profile",
+    component: Profile,
+  },
+  {
+    name: "projects",
+    path: "/projects",
+    component: Project,
+  },
+  {
+    name: "top",
+    path: "/",
+    component: Top,
+  },
+];
+
+export const createApp = ViteSSG(
+  // the root component
+  App,
+  // vue-router options
+  { routes },
+  // function to have custom setups
+  ({ app, router, routes, isClient, initialState }) => {
+    // install plugins etc.
+  }
+);
+
+// export async function includedRoutes(
+//   paths: string[],
+//   routes: RouteRecordRaw[]
+// ): Promise<string[]> {
+//   // Sensitive key is managed by Vite - this would not be available inside
+//   // vite.config.js as it runs before the environment has been populated.
+
+//   console.log({ articles });
+
+//   const newRoutes = routes.flatMap((route) => {
+//     return route.name === "articles"
+//       ? articles.map((article) => `/articles/${article.baseFileName}`)
+//       : route.path;
+//   });
+//   return newRoutes;
+// }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { readFile } from "fs/promises";
+import { join } from "path";
+import { RouteRecordRaw } from "vue-router";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // @ts-ignore
+  // https://github.com/antfu/vite-ssg/blob/main/src/types.ts
+  ssgOptions: {
+    dirStyle: "nested",
+    includedRoutes: async (paths: string[], routes: RouteRecordRaw[]) => {
+      const jsonData = await readFile(join("public", "contents.json"));
+      const {
+        articles,
+      }: {
+        articles: {
+          baseFileName: string;
+          title: string;
+          description: string;
+        }[];
+      } = JSON.parse(jsonData.toString());
+
+      const newRoutes = routes.flatMap((route) => {
+        return route.name === "articles"
+          ? articles.map((article) => `/articles/${article.baseFileName}`)
+          : route.path;
+      });
+      return newRoutes;
+    },
+  },
 });
