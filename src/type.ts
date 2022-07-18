494e135c8825f4a8af368c63155828906696e44c
sasakiy84 <sasakiy84@g.ecc.u-tokyo.ac.jp>
2022-07-18 12:26:37 +0900

    fix bug of heap out of memory

--- src/type.ts
+++ src/type.ts
@@ -3,8 +3,8 @@ import dayjs from "dayjs";
 export type articleFrontMatter = {
   title: string;
   description: string;
-  updatedAt: YearMonthDay;
-  createdAt: YearMonthDay;
+  updatedAt: string;
+  createdAt: string;
 };
 
 export const isArticleFroontMatter = (arg: any): arg is articleFrontMatter => {
@@ -28,10 +28,3 @@ export type articlesMetaRows = articleMetaRow[];
 export type getAllMetaRowsResponse = {
   articles: articlesMetaRows;
 };
-
-type oneToNine = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-type zeroToNine = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-type Year = `19${zeroToNine}${zeroToNine}` | `20${zeroToNine}${zeroToNine}`;
-type Month = `0${oneToNine}` | `1${0 | 1 | 2}`;
-type Day = `${0}${oneToNine}` | `${1 | 2}${zeroToNine}` | `3${0 | 1}`;
-export type YearMonthDay = `${Year}-${Month}-${Day}`;
