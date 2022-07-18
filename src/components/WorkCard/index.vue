9f3c1998dba8f1df5ad347c8471ca4f388351630
sasakiy84 <sasakiy84@g.ecc.u-tokyo.ac.jp>
2022-07-18 21:33:02 +0900

    add WorkCard

---
+++ src/components/WorkCard/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="card">
+    <div class="img-wrapper">
+      <img class="img" :src="src" alt="project image" />
+    </div>
+    <div class="text">
+      <p class="title">{{ title }}</p>
+      <p class="description">{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  description: string;
+  src: string;
+}>();
+</script>
+
+<style scoped lang="scss">
+@import "../../util.scss";
+.card {
+  width: 100%;
+  height: 160px;
+  border-left: 5px solid black;
+  padding: 10px 0 10px 10px;
+  display: flex;
+  flex-direction: row-reverse;
+  box-sizing: border-box;
+  margin-bottom: 30px;
+  @include sp {
+    display: block;
+    border-left: none;
+    padding: 10px;
+    height: max-content;
+  }
+  .img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    &-wrapper {
+      width: 250px;
+      height: 90%;
+      margin-top: auto;
+      margin-bottom: -10px;
+      @include sp {
+        width: 100%;
+        height: auto;
+        margin-bottom: 10px;
+      }
+    }
+  }
+  .text {
+    width: calc(100% - 250px);
+    padding-right: 30px;
+    @include sp {
+      width: 100%;
+    }
+    .title {
+      font-size: 24px;
+      font-weight: 400;
+      margin: 0 0 20px 0;
+      @include sp {
+        margin-bottom: 5px;
+      }
+    }
+    .description {
+      font-size: 13px;
+      line-height: 1.8;
+      overflow: hidden;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 4;
+      @include sp {
+        margin: 5px;
+      }
+    }
+  }
+}
+</style>
--- src/pages/WorkIndex.vue
+++ src/pages/WorkIndex.vue
@@ -1,13 +1,35 @@
 <template>
   <h1>Works</h1>
+  <article v-for="work in works" :key="work.title">
+    <WorkCard
+      :title="work.title"
+      :description="work.description"
+      :src="work.src"
+    ></WorkCard>
+  </article>
 </template>
 
 <script setup lang="ts">
 import { useHead } from "@vueuse/head";
 import { blogName } from "../utils/constant";
+import WorkCard from "../components/WorkCard/index.vue";
+
+const works = [
+  {
+    title: "個人ブログ",
+    description:
+      "ポートフォリオ&技術ブログ。UIライブラリとして Vue3 + vite 、を使用。 SSG したファイルを GitHub Actions を使用して AWS S3 にホスティングする構成。",
+    src: "https://i.ytimg.com/vi/o_RG03t7cVE/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBO_2ck7QU2z7S9CgE4gLbTm8vLEg",
+  },
+];
+
 useHead({
   title: `Works | ${blogName}`,
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  margin-bottom: 50px;
+}
+</style>
