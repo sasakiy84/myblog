00a3e717c69a03f315cf4e47f13fded8dc726950
sasakiy84 <67269692+sasakiy84@users.noreply.github.com>
2022-11-14 23:33:19 +0900

    Feature/link card (#7)
    * [wip] use puppeteer
    
    * fix: accelerate launch chrome
    
    * feat: SEO data

---
+++ src/jobs/generateLinkCardData.ts
@@ -0,0 +1,56 @@
+import path, { extname, join } from "path";
+import { mkdir, readdir, readFile, rm, writeFile } from "fs/promises";
+import { extractLinkCardData, parseMdToMdast } from "../utils/markdown";
+import { fetchSEOData } from "../utils/headlessBrowser";
+
+const articles = await readdir("articles");
+const fetchLinkPromimse = articles
+  .filter((filename) => {
+    return extname(filename) === ".md";
+  })
+  .map(async (filename): Promise<{ filename: string; links: string[] }> => {
+    const content = await readFile(join("articles", filename));
+    const root = parseMdToMdast(content.toString());
+    const links = extractLinkCardData(root);
+    return {
+      filename,
+      links,
+    };
+  });
+
+const linkCardData = (await Promise.allSettled(fetchLinkPromimse))
+  .map((result) => {
+    return result.status === "fulfilled" ? result.value : undefined;
+  })
+  .filter((d) => d);
+
+// console.log(linkCardData);
+
+const promises = linkCardData.map(async (data) => {
+  const seoData = await fetchSEOData(data?.links || []);
+  return {
+    filename: data?.filename,
+    seoData,
+  };
+});
+
+const result = await Promise.allSettled(promises);
+
+const METADATA_DIR_PATH = path.join("dist", "metadata");
+await rm(METADATA_DIR_PATH, { recursive: true, force: true });
+await mkdir(METADATA_DIR_PATH, { recursive: true });
+
+for (const data of result) {
+  if (data.status === "rejected" || data.value.filename === undefined) continue;
+
+  const basename = path.basename(
+    data.value.filename,
+    path.extname(data.value.filename)
+  );
+  await writeFile(
+    path.join(METADATA_DIR_PATH, `${basename}.json`),
+    JSON.stringify(data.value.seoData)
+  );
+  console.log(data.value.filename);
+  console.log(data.value.seoData);
+}
---
+++ src/utils/headlessBrowser.ts
@@ -0,0 +1,89 @@
+import puppeteer from "puppeteer-core";
+import { executablePath } from "puppeteer";
+
+export const fetchSEOData = async (
+  targetURLs: string[]
+): Promise<
+  {
+    title: string;
+    ogpURL: string;
+    description: string;
+  }[]
+> => {
+  console.log("launch browser");
+  const browser = await puppeteer.launch({
+    executablePath: executablePath(),
+    headless: true,
+    args: [
+      "--no-sandbox",
+      // "--single-process",
+      // "--no-first-run",
+      "--disable-dev-shm-usage",
+      "--disable-setuid-sandbox",
+      "--no-zygote",
+      "--disable-gpu",
+    ],
+  });
+  console.log("launched browser");
+
+  const page = await browser.newPage();
+  await page.setRequestInterception(true);
+  page.on("request", (interceptedRequest) => {
+    if (interceptedRequest.isInterceptResolutionHandled()) return;
+    if (
+      interceptedRequest.url().endsWith(".png") ||
+      interceptedRequest.url().endsWith(".jpg") ||
+      interceptedRequest.url().endsWith(".webp") ||
+      interceptedRequest.url().endsWith(".css")
+    )
+      interceptedRequest.abort();
+    else interceptedRequest.continue();
+  });
+
+  const result: {
+    title: string;
+    ogpURL: string;
+    description: string;
+    url: string;
+  }[] = [];
+
+  for (const url of targetURLs) {
+    try {
+      console.log(`go to page ${url}`);
+      await page.goto(url, { timeout: 0, waitUntil: "load" });
+      console.log("went to page");
+      const title = await page.title();
+      console.log("get title");
+      const descriptionHandle = await page.$("meta[property='og:description']");
+      const description =
+        (
+          await page.evaluate((descriptionElement) => {
+            return descriptionElement?.getAttribute("content");
+          }, descriptionHandle)
+        )?.replaceAll("\n", "") || "";
+      console.log("get desc");
+
+      descriptionHandle?.dispose();
+      const ogpURLHandle = await page.$("meta[property='og:image']");
+      const ogpURL =
+        (await page.evaluate((ogpElement) => {
+          return ogpElement?.getAttribute("content") || "";
+        }, ogpURLHandle)) || "";
+      ogpURLHandle?.dispose();
+      console.log("get ogp");
+
+      result.push({
+        title,
+        ogpURL,
+        description,
+        url,
+      });
+    } catch (e) {
+      console.log(`fetch ${url} failed`, e);
+    }
+  }
+
+  await browser.close();
+
+  return result;
+};
